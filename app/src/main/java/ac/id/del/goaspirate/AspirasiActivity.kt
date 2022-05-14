d38c874728d0856365c4a6c55bc25237ecbf33e1
rentasitorus <rentasitorus4@gmail.com>
2022-05-14 23:43:12 +0700

    popup

--- app/src/main/java/ac/id/del/goaspirate/AspirasiActivity.kt
+++ app/src/main/java/ac/id/del/goaspirate/AspirasiActivity.kt
@@ -4,8 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AspirasiActivity : AppCompatActivity() {
+
+//    init {
+//        val delete =  findViewById<AspirasiActivity>(R.id. deleteAspirasi)
+//
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aspirasi)
